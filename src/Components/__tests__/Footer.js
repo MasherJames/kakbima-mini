9449e5d20da2c9ca9fad75ac25f8ea912aeed0e9
James Macharia <jamesmasher13@gmail.com>
2020-09-05 01:20:32 +0300

    Add components test

---
+++ src/Components/__tests__/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "../../Utils/Tests/test-utils";
+
+import { Footer } from "..";
+
+it("Should render without error", () => {
+  render(<Footer />);
+});
---
+++ src/Components/__tests__/InputField.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "../../Utils/Tests/test-utils";
+
+import { InputField } from "..";
+
+it("Should render without error", () => {
+  render(<InputField />);
+});
